0f58bc596024d9d34765750a4ac71b1a74404283
uevh <bartolomyu12@gmail.com>
2022-04-12 13:02:56 +0700

    Initial commit

---
+++ WebApplication5/Controllers/MockupsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class MockupsController : Controller
+    {
+
+      
+
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            QuizModel qModel = QuizModel.Instance;
+            qModel.Reset();
+            qModel.Start();
+
+            
+            return View(qModel);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(QuizModel qModel, string action)
+        {
+            qModel = QuizModel.Instance;
+
+            if (ModelState.IsValid)
+
+            {
+                qModel.UserAnswer = Int32.Parse(Request.Form["UserAnswer"]);
+                qModel.Jeck();
+                
+
+
+                if (action == "Next")
+                {
+                    QuizModel quModel = QuizModel.Instance;
+                    quModel.Start();
+
+                    return View(quModel);
+                }
+                return RedirectToAction("QuizResult");
+            }
+            else
+            {
+               
+                QuizModel quModel = QuizModel.Instance;
+                return View(quModel);
+            }
+
+        }
+        public IActionResult QuizResult()
+        {
+            QuizModel qModel = QuizModel.Instance;
+            ViewBag.Result = qModel.AllAnswers;
+            ViewData["Total"] = "" + qModel.Count;
+            ViewData["Correct"] = "" + qModel.CountOfRightAnswers;
+            return View();
+        }
+
+
+    
+    }
+}
---
+++ WebApplication5/Models/QuizModel.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WebApplication5.Models
+{
+    public class QuizModel
+    {
+
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        //Собственный атрибут валидации
+        //[QuizOperation(new string[] { "+", "-", "*","/" })]
+        public string Operation { get; set; }
+        public double Solution { get; set; }
+        [Range(-100, 100)]
+        [Required]
+        public int UserAnswer { get; set; }
+
+        public string Answer { get; set; }
+        public List<string> AllAnswers;
+        public int Count;
+        public int CountOfRightAnswers;
+
+        public static QuizModel Instance { get; set; } = new QuizModel(1);
+        public void Reset()
+        {
+            Count = 0;
+            CountOfRightAnswers = 0;
+            AllAnswers = new List<string>();
+        }
+
+        public QuizModel()
+        {
+            
+        }
+        private QuizModel(int i)
+        {
+            
+        }
+        public void Start()
+        {
+            Random rand = new Random();
+            Number1 = rand.Next(0, 10);
+            Number2 = rand.Next(0, 10);
+            string[] OV = { "+", "-", "*", "/" };
+            Operation = OV[rand.Next(0, 3)];
+            Count++;
+
+        }
+
+
+        
+
+        public void Jeck()
+        {
+  
+            if (Operation == "+")
+            {
+                Solution = Number1 + Number2;
+                if (Solution == UserAnswer)
+                    CountOfRightAnswers++;
+                
+
+                AllAnswers.Add( "" + Number1 + " + " + Number2 + " = " + UserAnswer);
+            }
+            else if (Operation == "-")
+            {
+                Solution = Number1 - Number2;
+                if (Solution ==UserAnswer)
+                    CountOfRightAnswers++;
+               
+                AllAnswers.Add("" + Number1 + " - " + Number2 + " = " + UserAnswer);
+            }
+            else if (Operation == "*")
+            {
+                Solution = Number1 * Number2;
+                if (Solution == UserAnswer)
+                    CountOfRightAnswers++;
+                
+                AllAnswers.Add("" + Number1 + " * " + Number2 + " = " + UserAnswer);
+            }
+            else if (Operation == "/" && Number2 != 0)
+            {
+                Solution = Number1  /Number2;
+                if (Solution == UserAnswer)
+                    CountOfRightAnswers++;
+                
+                AllAnswers.Add("" + Number1 + " / " + Number2 + " = " + UserAnswer);
+            }
+            
+        }
+
+        public class QuizOperationAttribute : ValidationAttribute
+        {
+            
+            string[] _names;
+
+            public QuizOperationAttribute(string[] names)
+            {
+                _names = names;
+            }
+            public override bool IsValid(object value)
+            {
+                if (value != null && _names.Contains(value.ToString()))
+                    return true;
+
+                return false;
+            }
+        }
+
+    }
+}
